c7b483b1d3bcc5e28b6282c3f29d42a9b8e9b1af
Sarthak333 <sarthak333jha@gmail.com>
2019-02-11 17:05:48 +0530

    final

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -13,8 +13,13 @@ end
 
 def pnr
   @pnr=params[:store]
+  if Booking.where(pnr: @pnr).exists?
   @booking= Booking.where(pnr: @pnr).first
   render 'pnr'
+  else
+    flash[:danger]="PNR doesn't exist."
+    redirect_to root_path
+  end
 end
 
 private
--- app/models/phlight.rb
+++ app/models/phlight.rb
@@ -2,4 +2,6 @@ class Phlight < ApplicationRecord
   validates :name, presence: true
   validates :origin, presence: true
   validates :destination, presence: true
+  validates :fdate, presence: true
+  validates :ftime, presence: true
 end
